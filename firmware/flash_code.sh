563ad6d44809dc6e18df345b221f36c8abe70ef4
martinprest <mjprest451@gmail.com>
2023-04-02 17:11:28 +0100

    blink test

--- firmware/flash_code.sh
+++ firmware/flash_code.sh
@@ -1,2 +1,3 @@
 sudo kill `pgrep openocd`
-sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "program /tree/projects/github/psion-org-datapack-tool/firmware/build/datapack_tool.elf verify reset exit"
+#sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "program /tree/projects/github/psion-org-datapack-tool/firmware/build/datapack_tool.elf verify reset exit"
+sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "program /home/martin/Github/mp-psion-org-datapak-tool/firmware/build/datapak_tool.elf verify reset exit"
